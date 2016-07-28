4e5ca5b8657a6383fb84a6d0062e11aa07d67f1e
Yashvardhan Jain <j.yash00789@gmail.com>
2016-07-28 20:29:48 +0530

    Add files via upload

---
+++ loginScript.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("https://www.facebook.com")
+elem = driver.find_element_by_id("email")
+elem.send_keys("Username")
+elem1 = driver.find_element_by_id("pass")
+elem1.send_keys("Password")
+elem1.send_keys(Keys.ENTER)
+
+
